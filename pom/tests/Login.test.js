c0970a41e8db68f7716bbf67e6d4f89645630fce
Martha rodriguez <martha.rodriguez@wizeline.com>
2022-07-07 09:49:01 -0500

    changes Jul7th

--- pom/tests/Login.test.js
+++ pom/tests/Login.test.js
@@ -6,18 +6,18 @@ import taskPage from '../pages/TaskPage'
 fixture('Login feature test')
     .page`${URLS.LOGIN_URL}`
 
-test.skip('As a user, i should not be able to log in successfully when login form user and password are empty', async () => {
+test('As a user, i should not be able to log in successfully when login form user and password are empty', async () => {
     await t 
         .click(loginPage.loginButton)
         .expect(loginPage.labelUserPasswordEmpty.exists).ok()    
 })
 
-test.skip('As a user, i should not be able to log in successfully when providing only the password', async () => {
+test('As a user, i should not be able to log in successfully when providing only the password', async () => {
     await loginPage.submitLoginForm(" ", CREDENTIALS.STANDARD_USER.PASSWORD)
     await t.expect(loginPage.labelUserEmailEmpty.exists).ok()     
 })
 
-test.skip('As a user, i should not be able to log in successfully when providing invalid credentials', async () => {
+test('As a user, i should not be able to log in successfully when providing invalid credentials', async () => {
     await loginPage.submitLoginForm(INVALIDCREDENTIALS.INVALID_USER.INVALIDUSERNAME, INVALIDCREDENTIALS.INVALID_USER.INVALIDPASSWORD)
     await t.expect(loginPage.labelWrongCredentials.exists).ok()    
 })
