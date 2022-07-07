771fa8d49e5b5fc49bf1dabf594a35b63b07832f
Martha rodriguez <martha.rodriguez@wizeline.com>
2022-07-06 21:59:15 -0500

    changes Jul6th

--- pom/pages/TaskPage.js
+++ pom/pages/TaskPage.js
@@ -7,22 +7,34 @@ class TaskPage {
         this.addTaskHeader = Selector('div.DraftEditor-editorContainer > div')
         this.addTaskDescription = Selector('.f9408a0e')
         this.addTaskButton = Selector('._7a2031d6.a878a9a4')
-        this.taskName = Selector('.markdown_content task_content')
+        this.taskName = Selector('.markdown_content.task_content')
+    }
+
+    async createSingleTask() {
+        await t
+            .wait(100)
+            .click(this.plusAddButton)
+            .wait(100)
+            .typeText(this.addTaskHeader, 'SingleTask')
+            .wait(100)
+            .click(this.addTaskButton)
+            .wait(500)
+            .expect(this.taskName.withText('SingleTask').exists).ok()
     }
 
     async createTask(numberoftasks) {
         for (let i = 1; i <= numberoftasks; i++) {
             await t
-                .wait(1000)
+                .wait(100)
                 .click(this.plusAddButton)
-                .wait(1000)
+                .wait(100)
                 .typeText(this.addTaskHeader, `Task${i}`)
-                .wait(1000)
+                .wait(100)
                 .click(this.addTaskButton)
-                .wait(1000)
+                .wait(100)
+                .expect(this.taskName.withText(`Task${i}`).exists).ok()
         }  
     }
-  
 }
 
 
--- pom/tests/Createtasks.test.js
+++ pom/tests/Createtasks.test.js
@@ -1,13 +1,16 @@
-
 import loginPage from '../pages/LoginPage'
 import { CREDENTIALS, NUMBERTASKS, URLS } from '../data/Constants'
 import taskPage from '../pages/TaskPage'
 
-fixture('Login feature test')
+fixture('Create task feature test')
     .page`${URLS.LOGIN_URL}`
 
 test('As a user, i should be able to create a task', async () => {
-    await
-    loginPage.submitLoginForm(CREDENTIALS.STANDARD_USER.USERNAME, CREDENTIALS.STANDARD_USER.PASSWORD) 
-    taskPage.createTask(NUMBERTASKS.NUMBER_TASKS.NUMBEROFTASKS)
+    await loginPage.submitLoginForm(CREDENTIALS.STANDARD_USER.USERNAME, CREDENTIALS.STANDARD_USER.PASSWORD)
+    await taskPage.createSingleTask()
+})
+
+test('As a user, i should be able to create multiple tasks', async () => {
+    await loginPage.submitLoginForm(CREDENTIALS.STANDARD_USER.USERNAME, CREDENTIALS.STANDARD_USER.PASSWORD) 
+    await taskPage.createTask(NUMBERTASKS.NUMBER_TASKS.NUMBEROFTASKS)
 })
